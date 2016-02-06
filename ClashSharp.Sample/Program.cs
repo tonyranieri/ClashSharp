e64e954fee4330320d11982c59596211dfaafde7
Tony Ranieri <tony@ranieri.me>
2016-02-05 21:55:49 -0600

    added sample project

---
+++ ClashSharp.Sample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClashSharp.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

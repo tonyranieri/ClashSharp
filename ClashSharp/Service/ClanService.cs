7a2c124ad2e53ecd52937d4aa7d18cdc12bcb1a1
Tony Ranieri <tony@ranieri.me>
2016-02-05 22:46:12 -0600

    updated app config stuff, readme, added start of clan service impementation

---
+++ ClashSharp/Service/ClanService.cs
@@ -0,0 +1,26 @@
+﻿using ClashSharp.Data;
+using ClashSharp.Interface;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace ClashSharp.Service
+{
+    public class ClanService : IClanService
+    {
+        public ClanInformation GetClanInformation(string clanTag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ClanMember> ListClanMembers(string clanTag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ClanInformation> SearchClans(ClanSearchCriteria criteria)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -10,9 +10,10 @@ Create your token(s) here - https://developer.clashofclans.com/#/account
 Create a config file in the same location as your App.config / Web.config called `app.secrets.config`.  Put this config in the new file and set "value" to your API access token. 
 
 ```
-<configuration>
+<?xml version="1.0" encoding="utf-8" ?>
+<appSettings>
   <add key="clashAPIKey" value="" />
-</configuration>
+</appSettings>
 ```
 
 *Make sure you don't commit your token to source control!*

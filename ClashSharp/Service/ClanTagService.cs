feda804f561c2e9751cd6fa5c234e477ca6fefaf
Tony Ranieri <tony@ranieri.me>
2016-02-07 16:51:45 -0600

    added clan tag service to deal with clan tag stuff

---
+++ ClashSharp/Service/ClanTagService.cs
@@ -0,0 +1,20 @@
+﻿using ClashSharp.Interface;
+using System;
+
+namespace ClashSharp.Service
+{
+    public class ClanTagService : IClanTagService
+    {
+        public string EncodeClanTag(string clanTag)
+        {
+            // make sure our clan tag starts with a #
+            if (clanTag.StartsWith("#") == false)
+            {
+                throw new ApplicationException("Clan tag starts with '#'");
+            }
+
+            // encode our clanTag
+            return clanTag.Replace("#", "%23");
+        }
+    }
+}
